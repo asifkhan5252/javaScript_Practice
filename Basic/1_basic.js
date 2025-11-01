5dc0f906923e9a41f7faaadb40e53b6c63815fe4
asifkhan5252 <117203134+asifkhan5252@users.noreply.github.com>
2025-11-01 07:15:01 +0000

    only var

---
+++ Basic/1_basic.js
@@ -0,0 +1,10 @@
+console.log("hello")
+let score=4
+console.log(typeof score)
+
+let somenumber="4"
+console.log(typeof somenumber)
+
+let some=null
+console.log(typeof some)
+// typeof null is object
