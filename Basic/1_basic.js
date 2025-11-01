d3c09cfb7f013287614a51c5ac13d4ddcad02cf3
asifkhan5252 <117203134+asifkhan5252@users.noreply.github.com>
2025-11-01 07:41:52 +0000

    in basic

--- Basic/1_basic.js
+++ Basic/1_basic.js
@@ -1,10 +1,22 @@
 console.log("hello")
 let score=4
-console.log(typeof score)
+// console.log(typeof score)
 
 let somenumber="4"
-console.log(typeof somenumber)
+// console.log(typeof somenumber)
 
 let some=null
-console.log(typeof some)
+// console.log(typeof some)
 // typeof null is object
+
+// **************** operation in js******************************//
+let value=4
+let negvalue=-value
+console.log(negvalue)
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(4**3);
+// console.log(2/4);
+// console.log(40,50);
+
